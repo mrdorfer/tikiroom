0294038989058372986bc20498f29402e232d694
mrdorfer <92897708+mrdorfer@users.noreply.github.com>
2023-04-20 08:25:23 -0400

    Add files via upload

---
+++ tiki_v1.py
@@ -0,0 +1,23 @@
+# importing vlc, os, and random modules
+## this relies on the system having ffmpeg to play
+## music from cli
+##
+from gpiozero import MotionSensor
+from datetime import datetime 
+import os
+import time
+import random
+import path
+
+pir = MotionSensor(4)
+musichome="/home/bp/music/"
+
+while True:
+    pir.wait_for_motion()
+    randomsong = random.choice(os.listdir(musichome))
+    fullpath = (musichome + randomsong)
+    dt = datetime.now()
+    ts = datetime.timestamp(dt)
+    print('MOTION DETECTED, NOW PLAYING: ' +fullpath,ts)
+    os.system("ffplay -nodisp -autoexit "+fullpath+" >/dev/null 2>&1")
+    time.sleep(10)
