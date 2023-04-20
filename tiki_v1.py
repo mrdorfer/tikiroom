b84ae8cb79d24e5463472908f1e2ce346f47f6da
mrdorfer <92897708+mrdorfer@users.noreply.github.com>
2023-04-20 08:51:45 -0400

    Update tiki_v1.py
    cleaned up comments

--- tiki_v1.py
+++ tiki_v1.py
@@ -1,6 +1,4 @@
-# importing vlc, os, and random modules
-## this relies on the system having ffmpeg to play
-## music from cli.  Project between https://github.com/calubuddy and myself
+# Project between https://github.com/calubuddy and myself.  Switched to ffmpeg to play on a raspberry pi zero.
 ##
 from gpiozero import MotionSensor
 from datetime import datetime 
@@ -10,7 +8,7 @@ import random
 import path
 
 pir = MotionSensor(4)
-musichome="/home/music/"
+musichome="/home/tiki/music/"
 
 while True:
     pir.wait_for_motion()
@@ -18,6 +16,5 @@ while True:
     fullpath = (musichome + randomsong)
     dt = datetime.now()
     ts = datetime.timestamp(dt)
-    print('MOTION DETECTED, NOW PLAYING: ' +fullpath,ts)
     os.system("ffplay -nodisp -autoexit "+fullpath+" >/dev/null 2>&1")
     time.sleep(10)
